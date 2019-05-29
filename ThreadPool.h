2ca7993d10d19097a1e88628fdfde02d6a71a741
EricDDK <853003451@qq.com>
2019-05-29 13:38:30 +0800

    [Init] whole framework and cmake project.

---
+++ ThreadPool.h
@@ -0,0 +1,15 @@
+#ifndef _ASYNC_THREAD_POOL_H__
+#define _ASYNC_THREAD_POOL_H__
+
+class ThreadPool
+{
+public:
+	ThreadPool();
+	~ThreadPool();
+
+private:
+
+
+};
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+#include "ThreadPool.h"
+
+int main()
+{
+
+	return 1;
+}
