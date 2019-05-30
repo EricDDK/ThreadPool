40ca9cd2b6a818892d3c2f2da2059b5ec275dd09
EricDDK <853003451@qq.com>
2019-05-30 10:38:28 +0800

    [Add] a unit test that asynchronously passes the reference and gets the asynchronous reference result in the callback function.

--- test.cpp
+++ test.cpp
@@ -3,15 +3,19 @@
 
 void test1(int a, int b)
 {
-	// TODO
 	a = a + b;
 	return;
 }
 
 void test2(double a, double b)
 {
-	// TODO
 	a = a + b;
+	return;
+}
+
+void test3(int a, int b, int& result)
+{
+	result = a + b;
 	return;
 }
 
@@ -27,6 +31,14 @@ int main()
 	pool.add(std::bind(&test1, 1, 2), std::bind(&callback));
 	pool.add(std::bind(&test2, 1.0, 2.0), std::bind(&callback));
 	pool.add(std::bind(&test2, 1.0, 2.0));
+
+	//Add a unit test that asynchronously passes the reference and gets the asynchronous reference result in the callback function.
+	int result = 0;
+	pool.add(std::bind(&test3, 1.0, 2.0, std::ref(result)), [&]()
+	{
+		std::cout << "result = " << result << std::endl;
+	});
+
 	system("pause");
 	return 1;
 }
